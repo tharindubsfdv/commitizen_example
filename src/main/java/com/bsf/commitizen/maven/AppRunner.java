3522551cd65c8cb4f287b51220154281b7671829
Tharindu Eranga <tharindu.e@bsfdv.com>
2023-03-01 12:13:52 +0400

    fix: add the output to new method

--- src/main/java/com/bsf/commitizen/maven/AppRunner.java
+++ src/main/java/com/bsf/commitizen/maven/AppRunner.java
@@ -2,6 +2,10 @@ package com.bsf.commitizen.maven;
 
 public class AppRunner {
     public static void main(String[] args) {
-        System.out.println("Run me!");
+        myMethod();
+    }
+
+    public static void myMethod() {
+        System.out.println("MyMethod!");
     }
 }
