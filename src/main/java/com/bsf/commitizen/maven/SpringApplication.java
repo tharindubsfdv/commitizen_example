7b79deea745324debc9a3b5489f65b8804d8cd30
Tharindu Eranga <tharindu.e@bsfdv.com>
2023-03-01 13:20:42 +0400

    feat!: replace the old runner with SpringApplication

--- src/main/java/com/bsf/commitizen/maven/SpringApplication.java
+++ src/main/java/com/bsf/commitizen/maven/SpringApplication.java
@@ -1,6 +1,6 @@
 package com.bsf.commitizen.maven;
 
-public class MyApp {
+public class SpringApplication {
     public static void main(String[] args) {
         System.out.println("Add my app runner");
     }
