ecc8c953396347ab7073a1cfa8a9ed363f2ed325
Tharindu Eranga <tharindu.e@bsfdv.com>
2023-03-02 16:31:14 +0400

    feat: add a new line

--- src/main/java/com/bsf/commitizen/maven/SpringApplication.java
+++ src/main/java/com/bsf/commitizen/maven/SpringApplication.java
@@ -3,5 +3,6 @@ package com.bsf.commitizen.maven;
 public class SpringApplication {
     public static void main(String[] args) {
         System.out.println("Add my app runner");
+        System.out.println("Second update");
     }
 }
